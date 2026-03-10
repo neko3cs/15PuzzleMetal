fca82a9826218bd7ca2df91c5f3d5135106f218c
neko3cs <minaton.0722.net@gmail.com>
2026-03-10 14:12:34 +0900

    update: init Metal Swift Game Project

---
+++ 15PuzzleMetal/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  15PuzzleMetal
+//
+//  Created by neko3cs on 2026/03/10.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
---
+++ 15PuzzleMetalTests/_5PuzzleMetalTests.swift
@@ -0,0 +1,17 @@
+//
+//  _5PuzzleMetalTests.swift
+//  15PuzzleMetalTests
+//
+//  Created by neko3cs on 2026/03/10.
+//
+
+import Testing
+@testable import _5PuzzleMetal
+
+struct _5PuzzleMetalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
